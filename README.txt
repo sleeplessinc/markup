14b2492de77214b33aa3871d9d02bfed65dd6d51
Joe Hitchens <joe@sleepless.com>
2015-03-06 15:51:13 -0800

    -

--- README.txt
+++ README.txt
@@ -1,11 +1,8 @@
 
 Markup
 
-Sort of like Markdown, but parsed at run/load time on the browser.
+Sort of like Markdown, but parsed at run/load time on the browser and a lot less sophisticated.
 
 See index.html for example
-
-
-
 
 
--- markup.js
+++ markup.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2013 Sleepless Software Inc. All rights reserved.
+Copyright 2015 Sleepless Software Inc. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
@@ -21,67 +21,60 @@ IN THE SOFTWARE.
 */
 
 
-Markup = {
+Markup = {};
 
-	convert: function( t ) {
-		t = t.replace(/\r/gi, "\n");
+Markup.convert = function( t ) {
+	t = t.replace(/\r/gi, "\n");
 
-		// a blank line marks a paragraph
-		t = t.replace(/\n\n+/gi, "\n\n<p>\n");
-		
-		// headings h1 and h2
-		t = t.replace(/\n([^\s\n][^\n]+)\n={5,}\s*\n/gi, "\n<h1>$1</h1>\n" );
-		t = t.replace(/\n([^\s\n][^\n]+)\n-{5,}\s*\n/gi, "\n<h2>$1</h2>\n" );
+	// a blank line marks a paragraph
+	t = t.replace(/\n\n+/gi, "\n\n<p>\n");
+	
+	// headings h1 and h2
+	t = t.replace(/\n([^\s\n][^\n]+)\n={5,}\s*\n/gi, "\n<h1>$1</h1>\n" );
+	t = t.replace(/\n([^\s\n][^\n]+)\n-{5,}\s*\n/gi, "\n<h2>$1</h2>\n" );
 
-		// hyper link/anchor
-		t = t.replace(/\(\s*link\s+([^\s\)]+)\s*\)/gi, "(link $1 $1)");
-		t = t.replace(/\(\s*link\s+([^\s\)]+)\s*([^\)]+)\)/gi, "<a href=\"$1\">$2</a>");
+	// hyper link/anchor
+	t = t.replace(/\(\s*link\s+([^\s\)]+)\s*\)/gi, "(link $1 $1)");
+	t = t.replace(/\(\s*link\s+([^\s\)]+)\s*([^\)]+)\)/gi, "<a href=\"$1\">$2</a>");
 
-		// image
-		t = t.replace(/\(\s*image\s+([^\s\)]+)\s*\)/gi, "(image $1 $1)");
-		t = t.replace(/\(\s*image\s+([^\s\)]+)\s*([^\)]+)\)/gi, "<img src=\"$1\" title=\"$2\">");
-		// figure
-		t = t.replace(/\(\s*figure\s+([^\s\)]+)\s*\)/gi, "(figure $1 $1)");
-		t = t.replace(/\(\s*figure\s+([^\s\)]+)\s*([^\)]+)\)/gi, "<figure><img src=\"$1\" title=\"$2\"><figcaption>$2</figcaption></figure>");
+	// image
+	t = t.replace(/\(\s*image\s+([^\s\)]+)\s*\)/gi, "(image $1 $1)");
+	t = t.replace(/\(\s*image\s+([^\s\)]+)\s*([^\)]+)\)/gi, "<img src=\"$1\" title=\"$2\">");
+	// figure
+	t = t.replace(/\(\s*figure\s+([^\s\)]+)\s*\)/gi, "(figure $1 $1)");
+	t = t.replace(/\(\s*figure\s+([^\s\)]+)\s*([^\)]+)\)/gi, "<figure><img src=\"$1\" title=\"$2\"><figcaption>$2</figcaption></figure>");
 
-		t = t.replace(/__(([^_]|_[^_])*)__/gi, "<u>$1</u>");		// underline
-		t = t.replace(/\*\*(([^\*]|\*[^\*])*)\*\*/gi, "<em>$1</em>");	// emphasis
-		t = t.replace(/\n\s*"\s*\n([^"]+)"\s*\n/gi, "\n<blockquote>$1</blockquote>\n");	// blockquote
-		t = t.replace(/\n\s*{\s*\n([^"]+)}\s*\n/gi, "\n<blockquote><code>$1</code></blockquote>\n");
-		t = t.replace(/{([^}]+)}/gi, "<code>$1</code>");	// code
+	t = t.replace(/__(([^_]|_[^_])*)__/gi, "<u>$1</u>");		// underline
+	t = t.replace(/\*\*(([^\*]|\*[^\*])*)\*\*/gi, "<em>$1</em>");	// emphasis
+	t = t.replace(/\n\s*"\s*\n([^"]+)"\s*\n/gi, "\n<blockquote>$1</blockquote>\n");	// blockquote
+	t = t.replace(/\n\s*{\s*\n([^"]+)}\s*\n/gi, "\n<blockquote><code>$1</code></blockquote>\n");
+	t = t.replace(/{([^}]+)}/gi, "<code>$1</code>");	// code
 
-		// symbols
-		t = t.replace(/\(tm\)/gi, "&trade;");	
-		t = t.replace(/\(r\)/gi, "&reg;");	
-		t = t.replace(/\(c\)/gi, "&copy;");
-		t = t.replace(/\(cy\)/gi, "&copy;&nbsp;"+(new Date().getFullYear()));
+	// symbols
+	t = t.replace(/\(tm\)/gi, "&trade;");	
+	t = t.replace(/\(r\)/gi, "&reg;");	
+	t = t.replace(/\(c\)/gi, "&copy;");
+	t = t.replace(/\(cy\)/gi, "&copy;&nbsp;"+(new Date().getFullYear()));
 
-		// unordered list
-		t = t.replace(/\n((\s+-\s+[^\n]+\n)+)/gi, "\n<ul>\n$1\n</ul>");
-		t = t.replace(/\n\s+-\s+/gi, "\n<li>");
+	// unordered list
+	t = t.replace(/\n((\s+-\s+[^\n]+\n)+)/gi, "\n<ul>\n$1\n</ul>");
+	t = t.replace(/\n\s+-\s+/gi, "\n<li>");
 
-		// ordered list 
-		t = t.replace(/\n((\s+(\d+\.|#)\s+[^\n]+\n)+)/gi, "\n<ol>\n$1\n</ol>");
-		t = t.replace(/\n\s+(\d+\.|#)\s+/gi, "\n<li>");
+	// ordered list 
+	t = t.replace(/\n((\s+(\d+\.|#)\s+[^\n]+\n)+)/gi, "\n<ol>\n$1\n</ol>");
+	t = t.replace(/\n\s+(\d+\.|#)\s+/gi, "\n<li>");
 
-		// dashes
-		t = t.replace(/\n\s*-{4,}\s*\n/gi, "\n<hr>\n");		// horizontal rule
-		t = t.replace(/-{3}/gi, "&mdash;");		// mdash
-		t = t.replace(/-{2}/gi, "&ndash;");		// ndash
+	// dashes
+	t = t.replace(/\n\s*-{4,}\s*\n/gi, "\n<hr>\n");		// horizontal rule
+	t = t.replace(/-{3}/gi, "&mdash;");		// mdash
+	t = t.replace(/-{2}/gi, "&ndash;");		// ndash
 
-		if( navigator !== "undefined" ) {
-			// only supported if running in browser
-			t = t.replace(/\(\s*lastModified\s*\)/gi, document.lastModified);
-		}
-
-		return t;
+	if( navigator !== "undefined" ) {
+		// only supported if running in browser
+		t = t.replace(/\(\s*lastModified\s*\)/gi, document.lastModified);
 	}
 
+	return t;
 };
 
-/*
-String.prototype.markup = function() {
-	return Markup.convert( this );
-};
-*/
 
