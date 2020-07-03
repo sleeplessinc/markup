acf1a25cc5d47ed608f4447f6d1ab08cceba9cfa
Joe Hitchens <joe@sleepless.com>
2020-07-03 11:59:00 -0700

    -

--- README.txt
+++ README.txt
@@ -3,6 +3,9 @@ Markup
 
 Sort of like Markdown, but parsed at run/load time on the browser and a lot less sophisticated.
 
+I also think the syntax makes more sense and is more readable in txt form.
+I mostly use it for just formatting some very simple documents containing instructions or what have you.
+
 See index.html for example
 
 
--- markup.js
+++ markup.js
@@ -24,9 +24,17 @@ IN THE SOFTWARE.
 Markup = {};
 
 Markup.convert = function( t ) {
-	t = t.replace(/\r/gi, "\n");
 
-	// a blank line marks a paragraph
+	// nuke CRs
+	t = t.replace(/\r/gi, "\n")
+
+	// remove leading trailing whitespace on all lines
+	t = t.split( /\n/ ).map( l => l.trim() ).join( "\n" );
+
+	// prepend a couple newlines so that regexps below will match at the beginning.
+	t = "\n\n" + t;		// note: will cause a <p> to always appear at start of output
+
+	// one or more blank lines mark a paragraph
 	t = t.replace(/\n\n+/gi, "\n\n<p>\n");
 	
 	// headings h1 and h2
@@ -37,9 +45,14 @@ Markup.convert = function( t ) {
 	t = t.replace(/\(\s*link\s+([^\s\)]+)\s*\)/gi, "(link $1 $1)");
 	t = t.replace(/\(\s*link\s+([^\s\)]+)\s*([^\)]+)\)/gi, "<a href=\"$1\">$2</a>");
 
+	// hyper link/anchor that opens in new window/tab
+	t = t.replace(/\(\s*xlink\s+([^\s\)]+)\s*\)/gi, "(xlink $1 $1)");
+	t = t.replace(/\(\s*xlink\s+([^\s\)]+)\s*([^\)]+)\)/gi, "<a target=_blank href=\"$1\">$2</a>");
+
 	// image
 	t = t.replace(/\(\s*image\s+([^\s\)]+)\s*\)/gi, "(image $1 $1)");
 	t = t.replace(/\(\s*image\s+([^\s\)]+)\s*([^\)]+)\)/gi, "<img src=\"$1\" title=\"$2\">");
+
 	// figure
 	t = t.replace(/\(\s*figure\s+([^\s\)]+)\s*\)/gi, "(figure $1 $1)");
 	t = t.replace(/\(\s*figure\s+([^\s\)]+)\s*([^\)]+)\)/gi, "<figure><img src=\"$1\" title=\"$2\"><figcaption>$2</figcaption></figure>");
@@ -50,19 +63,20 @@ Markup.convert = function( t ) {
 	t = t.replace(/\n\s*{\s*\n([^"]+)}\s*\n/gi, "\n<blockquote><code>$1</code></blockquote>\n");
 	t = t.replace(/{([^}]+)}/gi, "<code>$1</code>");	// code
 
-	// symbols
+	// special
 	t = t.replace(/\(tm\)/gi, "&trade;");	
 	t = t.replace(/\(r\)/gi, "&reg;");	
 	t = t.replace(/\(c\)/gi, "&copy;");
 	t = t.replace(/\(cy\)/gi, "&copy;&nbsp;"+(new Date().getFullYear()));
+	t = t.replace(/\(cm\s([^)]+)\)/gi, "&copy;&nbsp;"+(new Date().getFullYear())+"&nbsp;$1&nbsp;&ndash;&nbsp;All&nbsp;Rights&nbsp;Reserved" )
 
 	// unordered list
-	t = t.replace(/\n((\s+-\s+[^\n]+\n)+)/gi, "\n<ul>\n$1\n</ul>");
-	t = t.replace(/\n\s+-\s+/gi, "\n<li>");
+	t = t.replace(/\n((\s*-\s+[^\n]+\n)+)/gi, "\n<ul>\n$1\n</ul>");
+	t = t.replace(/\n\s*-\s+/gi, "\n<li>");
 
 	// ordered list 
-	t = t.replace(/\n((\s+(\d+\.|#)\s+[^\n]+\n)+)/gi, "\n<ol>\n$1\n</ol>");
-	t = t.replace(/\n\s+(\d+\.|#)\s+/gi, "\n<li>");
+	t = t.replace(/\n((\s*(\d+\.|#)\s+[^\n]+\n)+)/gi, "\n<ol>\n$1\n</ol>");
+	t = t.replace(/\n\s*(\d+\.|#)\s+/gi, "\n<li>");
 
 	// dashes
 	t = t.replace(/\n\s*-{4,}\s*\n/gi, "\n<hr>\n");		// horizontal rule
